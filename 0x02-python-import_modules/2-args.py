e03e13af8161ab371aad4e5bacc7a2ee60a7b11b
IsaacKunmi <kunmiolamiju@gmail.com>
2022-12-02 07:41:05 -0800

    Learning python: dynamic script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,14 @@ import sys
 elements = sys.argv
 count = len(sys.argv)
 rcount = count - 1
-if rcount == 1:
-    print(f"1 argument:")
-    for a in range(1, rcount):
+
+if __name__ == "__main__":
+    if rcount == 1:
+        print(f"1 argument:")
+        for a in range(1, rcount):
+            print(f"{a}: {elements[a]}")
+
+    else:
+        print(f"{rcount} arguments:")
+    for a in range(1, count):
         print(f"{a}: {elements[a]}")
-else:
-    print(f"{rcount} arguments:")
-for a in range(1, count):
-    print(f"{a}: {elements[a]}")
