f01f9cbe229f5dde4a0832dc0476615a795005b6
IsaacKunmi <kunmiolamiju@gmail.com>
2022-12-03 12:54:48 -0800

    Learning python: Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,7 +4,8 @@ numbers = sys.argv
 count = len(numbers)
 total = 0
 
-for a in range(1, count):
-    total += int(numbers[a])
+if __name__ == "__main__":
+    for a in range(1, count):
+        total += int(numbers[a])
 
-print(f"{total}")
+    print(f"{total}")
