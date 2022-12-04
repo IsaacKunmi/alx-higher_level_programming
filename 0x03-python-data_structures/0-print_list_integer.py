01dd28a90f6746e8482452185e952877196ade99
IsaacKunmi <kunmiolamiju@gmail.com>
2022-12-04 12:59:31 -0800

    Learning Python: Manipulating lists

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    my_list.reverse()
-    for a in range(0, len(my_list)):
-        print("{}".format(my_list.pop()))
-        print("\n")
+    for a in range(len(my_list)):
+        print("{}".format(my_list[a]))
