5989b7f3b618c6859ee3a9c80cb17e36bdef4650
IsaacKunmi <kunmiolamiju@gmail.com>
2022-12-06 03:30:16 -0800

    Learnig Python: remove characters from string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    expempt =['c','C']
+    expempt = ['c', 'C']
     sliced_string = "".join(a for a in my_string if a not in expempt)
     return sliced_string
