80d303874850712aef991a048e209072aa78c752
IsaacKunmi <kunmiolamiju@gmail.com>
2022-12-07 13:04:31 -0800

    Learning Python: Appending to List

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    if my_list is None:
+        return
     multiple_of_2 = []
-    for a in range(len(my_list)):
+    for a in my_list:
         if my_list[a] % 2 == 0:
-            multiple_of_2.append(my_list[a])
-
+            multiple_of_2.append(True)
+        else:
+            multiple_of_2.append(False)
         return multiple_of_2
