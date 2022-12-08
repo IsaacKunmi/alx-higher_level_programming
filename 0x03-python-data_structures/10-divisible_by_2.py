054e99d6e634a968a8ff056da510390c4a7605ff
IsaacKunmi <kunmiolamiju@gmail.com>
2022-12-08 05:34:25 -0800

    Learning Python: Iterate over a list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-
     multiple_of_2 = []
-    if my_list is None:
-        for a in my_list:
-            if my_list[a] % 2 == 0:
-                multiple_of_2.append(True)
-            return multiple_of_2
-    else:
-        multiple_of_2.append(False)
-    return multiple_of_2
+    for a in my_list:
+        if my_list[a] % 2 == 0:
+            multiple_of_2.append(True)
+        else:
+             multiple_of_2.append(False)
+        return multiple_of_2
