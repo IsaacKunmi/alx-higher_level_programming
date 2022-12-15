21e9efe48be355671d01a8dcd7583ea654df6161
IsaacKunmi <kunmiolamiju@gmail.com>
2022-12-15 04:38:29 -0800

    Learning Python: replace occurences of an element by another in a new list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = []
-    for a in my_list:
-        if a == search:
-            new_list.append(replace)
-        else:
-            new_list.append(a)
+    new_list = [replace if a == search else a for a in my_list]
     return new_list
