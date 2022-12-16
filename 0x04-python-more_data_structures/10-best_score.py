437b4844f80bf81eec2cc4ff6d2dd173ae8c342c
IsaacKunmi <kunmiolamiju@gmail.com>
2022-12-16 01:04:24 -0800

    Learning Python: Return a key with the biggest integer value (dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
+
     best_key = list(a_dictionary.keys())[0]
-    best_value = a_dictionary[first_key]
+    best_value = a_dictionary[best_key]
     for key, value in a_dictionary.items():
         if value > best_value:
             best_value = value
